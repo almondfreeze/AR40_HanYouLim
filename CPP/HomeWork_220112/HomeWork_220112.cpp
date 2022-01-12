78528a3a601912b7b81cb774b062b956bb90a039
유림 한 <y090724@naver.com>
2022-01-13 01:03:46 +0900

    한유림 : 220112숙제커밋

---
+++ CPP/HomeWork_220112/HomeWork_220112.cpp
@@ -0,0 +1,32 @@
+﻿// HomeWork_220112.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	{
+		//짝수
+		int Arr[10] = { 0, };
+		int a = 0;
+		while (a < 10)
+		{
+			Arr[a] = (a * 2) + 2;
+			a += 1;
+		}
+
+	}
+
+	{
+		//홀수
+		int Arr[10] = { 0, };
+		int A = 0;
+		while (A < 10)
+		{
+			Arr[A] = (A * 2) + 1;
+			A += 1;
+		}
+
+		int a = 0;
+	}
+}
